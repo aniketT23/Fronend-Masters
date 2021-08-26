580e1f431159a30831f4dd05954e79e4ca4ac606
Aniket <tiwarianiket91@gmail.com>
2021-08-26 18:54:05 +0530

    particular course 1

--- Courses_Page/myscript.js
+++ Courses_Page/myscript.js
@@ -2,6 +2,7 @@ let aCourse = document.getElementById("aCourse");
 let pCourse = document.getElementById("pCourse");
 let lCourse = document.getElementById("lCourse");
 let content = document.getElementById("course-content");
+let pop = document.getElementById("popular-main");
 
 function backgroundChange() {
     aCourse.style.background = "none";
@@ -14,9 +15,18 @@ function allCourse() {
     aCourse.style.background = "rgb(" + 10 + "," + "87" + "," + 163 + ")";
     aCourse.style.color = "white";
     content.style.display = "grid";
+    pop.style.display = "none";
 }
 
 
 pCourse.addEventListener("click", backgroundChange);
 lCourse.addEventListener("click", backgroundChange);
 aCourse.addEventListener("click", allCourse);
+
+
+function popularCourse() {
+
+    pop.style.display = "grid";
+
+}
+pCourse.addEventListener("click", popularCourse);
